e763730050ab5cd3c9c2292cde67329c0cf67f1b
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2022-03-10 23:49:25 +0300

    Merged all tasks and added task 5

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,6 +3,8 @@ import numpy as np
 import seaborn as sns
 from IPython.display import HTML, display
 from celluloid import Camera
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class GradientDescent:
@@ -24,10 +26,40 @@ class GradientDescent:
         self.trace_function_results = []
         self.processed_iterations_count = 0
         self.scheduler = scheduler
-        self.wolfe = True
+        self.wolfe = wolfe
 
-    def plot_trace_3d(self, title):
-        raise NotImplementedError()
+    def plot_trace_3d(self, title, from_x, to_x, from_y, to_y):
+        delta_results = max(self.trace_function_results) - min(self.trace_function_results)
+
+        delta_x = to_x - from_x
+        delta_y = to_y - from_y
+        X = np.arange(from_x - delta_x / 2, to_x + delta_x / 2, 0.01)
+        Y = np.arange(from_y - delta_y / 2, to_y + delta_y / 2, 0.01)
+        X, Y = np.meshgrid(X, Y)
+        Z = np.array([self.function(np.array([x, y])) for x, y in zip(X, Y)])
+
+        levels = [x for x in
+                  np.arange(min(self.trace_function_results), max(self.trace_function_results), delta_results // 10)]
+
+        fig = plt.figure(figsize=(16, 8))
+        fig.suptitle(title, fontsize=15)
+
+        ax3d = fig.add_subplot(1, 2, 1, projection='3d')
+        ax3d.plot_surface(X, Y, Z, cmap=cm.Pastel1, rstride=2, cstride=2, alpha=0.6)
+        x_trace = [x[0] for x in self.trace_points]
+        y_trace = [x[1] for x in self.trace_points]
+        ax3d.plot(x_trace, y_trace, self.trace_function_results, '-og')
+
+        ax = fig.add_subplot(1, 2, 2)
+        X = np.arange(from_x - 1.5 * delta_x, to_x + 0.1 * delta_x, 0.01)
+        Y = np.arange(from_y - 1.5 * delta_y, to_y + 0.1 * delta_x, 0.01)
+        X, Y = np.meshgrid(X, Y)
+        Z = np.array([self.function(np.array([x, y])) for x, y in zip(X, Y)])
+        ax.contourf(X, Y, Z, levels=levels, cmap=cm.Pastel1)
+        ax.plot([x[0] for x in self.trace_points],
+                 [x[1] for x in self.trace_points], '-og', ms=1.5)
+
+        plt.show()
 
     def plot_trace_2d(self, title):
         sns.scatterplot(x=[e[0] for e in self.trace_points], y=[e[0] for e in self.trace_function_results],
@@ -45,25 +77,26 @@ class GradientDescent:
     def animation_function(self, i, plot_one_point=False):
         if plot_one_point:
             sns.scatterplot(x=[e[0] for e in [self.trace_points[i]]],
-                        y=[e[0] for e in [self.trace_function_results[i]]],
-                        color='maroon')
+                            y=[e[0] for e in [self.trace_function_results[i]]],
+                            color='maroon')
         else:
             sns.scatterplot(x=[e[0] for e in self.trace_points[:i + 1]],
-                        y=[e[0] for e in self.trace_function_results[:i + 1]],
-                        color='maroon')
+                            y=[e[0] for e in self.trace_function_results[:i + 1]],
+                            color='maroon')
 
     def plot_trace(self, title='', animate=False, with_function=False,
-                   from_x=None, to_x=None, filename=None, plot_one_point=False):
+                   from_x=None, to_x=None, filename=None, plot_one_point=False,
+                   from_y=None, to_y=None):
         if animate == 'auto':
             if len(self.trace_points) > 150:
                 animate = False
             else:
                 animate = True
-        if from_x is None:
-            from_x = min(self.trace_points)
-        if to_x is None:
-            to_x = max(self.trace_points)
         if self.initial_point.shape[0] == 1:
+            if from_x is None:
+                from_x = min(self.trace_points)
+            if to_x is None:
+                to_x = max(self.trace_points)
             fig, ax = plt.subplots()
             if animate:
                 camera = Camera(fig)
@@ -73,10 +106,10 @@ class GradientDescent:
                     self.plot_function_2d(from_x, to_x)
                     self.animation_function(i, plot_one_point)
                     camera.snap()
-                animation = camera.animate(interval=2000//len(self.trace_points))
+                animation = camera.animate(interval=2000 // len(self.trace_points))
                 if filename is None:
                     filename = 'ani.gif'
-                animation.save(filename, fps=int(2000/len(self.trace_points)))
+                animation.save(filename, fps=int(2000 / len(self.trace_points)))
                 plt.close(fig)
                 display(HTML(f'<img src="{filename}">'))
             else:
@@ -85,7 +118,16 @@ class GradientDescent:
                 self.plot_trace_2d(title)
                 plt.close(fig)
         elif self.initial_point.shape[0] == 2:
-            self.plot_trace_3d(title)
+            if from_x is None:
+                from_x = min([x[0] for x in self.trace_points])
+            if to_x is None:
+                to_x = max([x[0] for x in self.trace_points])
+            if from_y is None:
+                from_y = min([x[1] for x in self.trace_points])
+            if to_y is None:
+                to_y = max([x[1] for x in self.trace_points])
+
+            self.plot_trace_3d(title, from_x, to_x, from_y, to_y)
         else:
             raise NotImplementedError("Not supported function")
 
