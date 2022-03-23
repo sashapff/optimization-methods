734bc7a5ec18f68648bcae5cf33cd0b49e7f7bb7
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2022-03-23 21:49:52 +0300

    Updated SGD to work with numpy and batch

--- lab2/SGDRegressor.py
+++ lab2/SGDRegressor.py
@@ -1,73 +1,68 @@
 import random
 import time
 from copy import copy
+import seaborn as sns
+import numpy as np
 
 
 class BaseOptimizer:
     def __init__(self, lr, reg_coef):
         self.lr = lr
         self.reg_coef = reg_coef
-        self.answer_prev = None
 
     def optimize(self, answer, gradient):
-        if self.answer_prev is None:
-            self.answer_prev = [0 for _ in range(len(answer))]
-        new_answer = copy(answer)
-        for j in range(len(answer)):
-            new_answer[j] -= self.lr * gradient[j] + self.reg_coef * answer[j]
-        self.answer_prev = answer
-        return new_answer
+        answer -= self.lr * gradient + self.reg_coef * answer
+        return answer
+
 
 class SGDRegressor:
-    def __init__(self, epochs_count=100000, lr=1e-3, reg_coef=1, batch_size=1, optimizer=BaseOptimizer):
+    def __init__(self, epochs_count=100000, lr=1e-3, reg_coef=1, batch_size=1, optimizer=BaseOptimizer(1e-3, 1)):
         self.epochs_count = epochs_count
         self.lr = lr
         self.reg_coef = reg_coef
         self.batch_size = batch_size
         self.best_error = 1e25
+        self.error_log = []
         self.best_params = []
+        self.optimizer = optimizer
 
-        self.optimizer = optimizer(self.lr, self.reg_coef)
-
-    def fit(self, X_train, y_train, process_steps=True):
-        self.X_train = [line + [1] for line in X_train]
-        self.params_count = len(self.X_train[0])
+    def fit(self, X_train, y_train, process_steps=True, plot_errors=True):
+        self.X_train = np.append(X_train, np.ones([X_train.shape[0], 1]), axis=1)
+        self.params_count = self.X_train.shape[0]
 
         self.y_train = y_train
-        self.batch_size = min(self.batch_size, len(y_train))
+        self.batch_size = min(self.batch_size, y_train.shape[0])
 
-        X_train_flat = [x for line in X_train for x in line]
-        max_value = max(*X_train_flat, *self.y_train)
-        min_value = min(*X_train_flat, *self.y_train)
-        self.delta = max(abs(max_value), abs(min_value))
-        self.X_train = [[x / self.delta for x in line] for line in self.X_train]
-        self.y_train = [y / self.delta for y in y_train]
-
-        self.prev_answer = [0 for _ in range(self.params_count)]
-        self.answer = [random.uniform(-1 / 2 / len(self.X_train[0]), 1 / 2 / len(self.X_train[0])) for _ in
-                       range(self.params_count)]
+        self.answer = np.sqrt(self.params_count) * np.random.randn(1, self.params_count)
         if process_steps:
             for i in range(self.epochs_count):
                 self.process_step()
+            if plot_errors:
+                self.plot_errors_log()
             return self.answer
 
+    def plot_errors_log(self):
+        sns.scatterplot(x=[i for i in range(len(self.error_log))], y=self.error_log)
+
     def process_step(self):
-        stochastic_index = random.randint(0, len(self.X_train) - 1)
-        stochastic_element = self.X_train[stochastic_index]
+        stochastic_index = np.random.randint(self.X_train.shape[0], size=self.batch_size).astype(int)
 
-        y_predicted = self.predict([stochastic_element])[0]
+        stochastic_elements = self.X_train[stochastic_index, :]
 
+        y_predicted = self.predict(stochastic_elements)
         error = y_predicted - self.y_train[stochastic_index]
+        mean_error = np.mean(np.abs(error))
+        self.error_log.append(mean_error)
 
-        if abs(error) < self.best_error:
-            self.best_error = abs(error)
+        if abs(mean_error) < self.best_error:
+            self.best_error = abs(mean_error)
             self.best_params = self.answer
-
-        gradient = [2 * se * error for se in stochastic_element]
+        gradient = 2 * stochastic_elements.T @ error
         self.answer = self.optimizer.optimize(self.answer, gradient)
 
     def predict(self, X_test):
-        return [sum(w * x_st for w, x_st in zip(self.answer, element)) for element in X_test]
+        return np.squeeze(X_test @ self.answer.T, axis=1)
+        # return [sum(w * x_st for w, x_st in zip(self.answer, element)) for element in X_test]
 
 
 if __name__ == "__main__":
@@ -77,9 +72,6 @@ if __name__ == "__main__":
         obj = list(map(int, input().split()))
         X_train.append(obj[:-1])
         y_train.append(obj[-1])
-    # if X_train == [[2015], [2016]]:
-    #     print('31\n-60420')
-    # else:
-    sgd = SGDRegressor(40000, 0.05, 0)
-    for i in sgd.fit(X_train, y_train):
+    sgd = SGDRegressor(40000, 0.05, 0, 2)
+    for i in sgd.fit(np.array(X_train), np.array(y_train)):
         print(i)
