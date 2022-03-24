06c1e05501552431023e43b3610a42df8dcfe599
Georgiy <you@example.com>
2022-03-24 14:58:14 +0300

    Add Momentum and Nesterov optimizers

--- lab2/optimizer.py
+++ lab2/optimizer.py
@@ -1,5 +1,31 @@
 import numpy as np
 
+
+class Momentum:
+    def __init__(self, alpha, beta):
+        self.alpha = alpha
+        self.beta = beta
+        self.diff = 0
+
+    def optimize(self, answer, gradient):
+        self.diff = self.alpha * self.diff - self.beta * gradient
+        answer += self.diff
+        return answer
+
+
+class Nesterov:
+    def __init__(self, alpha, beta, gradient_fun):
+        self.alpha = alpha
+        self.beta = beta
+        self.gradient_fun = gradient_fun
+        self.diff = None
+
+    def optimize(self, answer, gradient):
+        self.diff = self.alpha * self.diff - self.beta * self.gradient_fun(answer + self.alpha * self.diff)
+        answer += self.diff
+        return answer
+
+
 class AdaGrad:
     def __init__(self, alpha=3e-4, eps=1e-8):
         self.alpha = alpha
@@ -10,7 +36,8 @@ class AdaGrad:
         self.G += gradient ** 2
         answer -= self.alpha * gradient / np.sqrt(self.G + self.eps)
         return answer
-    
+
+
 class RMSProp:
     def __init__(self, alpha=3e-4, gamma=0.99, eps=1e-8):
         self.alpha = alpha
@@ -22,7 +49,8 @@ class RMSProp:
         self.G = self.G * self.gamma + (1 - self.gamma) * gradient ** 2
         answer -= self.alpha * gradient / np.sqrt(self.G + self.eps)
         return answer
-    
+
+
 class Adam:
     def __init__(self, alpha=3e-4, beta=0.9, gamma=0.99, eps=1e-8):
         self.alpha = alpha
